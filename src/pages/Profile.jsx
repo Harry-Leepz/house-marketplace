5588244d57ffff93f120d233e3271d4a935787fa
Harry Dhillon <hdhillon478@gmail.com>
2024-01-08 15:56:22 +0000

    Update profile component with basic structure

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
 
 export const Profile = () => {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    setUser(auth.currentUser);
+  }, []);
+
+  return user ? <div>{user.displayName}</div> : <div>Not logged in </div>;
 };
