d1c066b9207f55c5296e26011996d98b357133f6
Harry Dhillon <hdhillon478@gmail.com>
2024-01-16 16:40:01 +0000

    Add loading component to private route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 
+import { Loading } from "./Loading";
 import { useAuthStatus } from "../hooks/useAuthStatus";
 
 export const PrivateRoute = () => {
   const { loading, loggedIn } = useAuthStatus();
 
   if (loading) {
-    return <h1>Loading...</h1>;
+    return <Loading />;
   }
 
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
