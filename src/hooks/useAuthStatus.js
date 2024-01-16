e1ba7946aad1b0bbb6c0ca3c85d3708058d27153
Harry Dhillon <hdhillon478@gmail.com>
2024-01-16 16:33:16 +0000

    Add fix for react memory leak error

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,21 +1,29 @@
-import { useEffect, usestate } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // https://stackoverflow.com/questions/65505665/protected-route-with-firebase
 // custom hook to check the user's auth status
 export const useAuthStatus = () => {
-  const [loggedIn, setLoggedIn] = usestate(false);
-  const [loading, setLoading] = usestate(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
-      setLoading(false);
-    });
-  });
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setLoading(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return { loading, loggedIn };
 };
