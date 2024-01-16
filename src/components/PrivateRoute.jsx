21547e7028a92f7710a5c28aed983899c0b7559e
Harry Dhillon <hdhillon478@gmail.com>
2024-01-16 16:33:51 +0000

    Update private router with auth status hook

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 
+import { useAuthStatus } from "../hooks/useAuthStatus";
+
 export const PrivateRoute = () => {
-  const loggedIn = false;
+  const { loading, loggedIn } = useAuthStatus();
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
 };
