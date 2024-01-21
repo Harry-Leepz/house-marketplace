f8fda714b1a9784188f26d71ab36aae76e71bb1a
Harry Dhillon <hdhillon478@gmail.com>
2024-01-21 19:45:37 +0000

    Start to build out the explore component page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,3 +1,36 @@
+import { Link } from "react-router-dom";
+
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+
 export const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
+      <main>
+        {/* Image Slider Goed Here */}
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt='Inside of an apartment'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for Rent</p>
+          </Link>
+          <Link to='/category/sell'>
+            <img
+              src={sellCategoryImage}
+              alt='Outside of a house'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for Sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 };
