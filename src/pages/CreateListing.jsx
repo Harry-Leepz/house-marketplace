ccf98309a043490dcc3cc3ea1cc3ac2e14c5a670
Harry Dhillon <hdhillon478@gmail.com>
2024-01-30 21:12:41 +0000

    Complete adding jsx contect for create lasting page component

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -171,6 +171,147 @@ export const CreateListing = () => {
               No
             </button>
           </div>
+
+          {/* Furnished */}
+          <label className='formLabel'>Furnished</label>
+          <div className='formButtons'>
+            <button
+              className={furnished ? "formButtonActive" : "formButton"}
+              type='button'
+              id='furnished'
+              value={true}
+              onClick={onMutateHandler}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !furnished && furnished !== null
+                  ? "formButtonActive"
+                  : "formButton"
+              }
+              type='button'
+              id='furnished'
+              value={false}
+              onClick={onMutateHandler}
+            >
+              No
+            </button>
+          </div>
+
+          {/* Address */}
+          <label className='formLabel'>Address</label>
+          <textarea
+            className='formInputAddress'
+            type='text'
+            id='address'
+            value={address}
+            onChange={onMutateHandler}
+            required
+          />
+
+          {!geolocationEnabled && (
+            <div className='formLatLng flex'>
+              <div>
+                <label className='formLabel'>Latitude</label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='latitude'
+                  value={latitude}
+                  onChange={onMutateHandler}
+                  required
+                />
+              </div>
+              <div>
+                <label className='formLabel'>Longitude</label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='longitude'
+                  value={longitude}
+                  onChange={onMutateHandler}
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+          {/* Offer */}
+          <label className='formLabel'>Offer</label>
+          <div className='formButtons'>
+            <button
+              className={offer ? "formButtonActive" : "formButton"}
+              type='button'
+              id='offer'
+              value={true}
+              onClick={onMutateHandler}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !offer && offer !== null ? "formButtonActive" : "formButton"
+              }
+              type='button'
+              id='offer'
+              value={false}
+              onClick={onMutateHandler}
+            >
+              No
+            </button>
+          </div>
+
+          {/* Pricing */}
+          <label className='formLabel'>Regular Price</label>
+          <div className='formPriceDiv'>
+            <input
+              className='formInputSmall'
+              type='number'
+              id='regularPrice'
+              value={regularPrice}
+              onChange={onMutateHandler}
+              min='50'
+              max='750000000'
+              required
+            />
+            {type === "rent" && <p className='formPriceText'>$ / Month</p>}
+          </div>
+
+          {offer && (
+            <>
+              <label className='formLabel'>Discounted Price</label>
+              <input
+                className='formInputSmall'
+                type='number'
+                id='discountedPrice'
+                value={discountedPrice}
+                onChange={onMutateHandler}
+                min='50'
+                max='750000000'
+                required={offer}
+              />
+            </>
+          )}
+
+          {/* Image Upload */}
+          <label className='formLabel'>Images</label>
+          <p className='imagesInfo'>
+            The first image will be the cover (max 6).
+          </p>
+          <input
+            className='formInputFile'
+            type='file'
+            id='images'
+            onChange={onMutateHandler}
+            max='6'
+            accept='.jpg,.png,.jpeg'
+            multiple
+            required
+          />
+          <button type='submit' className='primaryButton createListingButton'>
+            Create Listing
+          </button>
         </form>
       </main>
     </div>
